5ff05aab26211cc2ea2649045c05ac854f9c3017
LunaRossie <>
2022-08-15 00:55:17 -0400

    add to index file

--- README.md
+++ README.md
@@ -7,41 +7,46 @@ Table of Contents
 # Github 
 # email address
 # Video Link
-# Hobbies
 # Language
 # License
 
 
-Description
+## Description
 
 I've created a READme Generator to enhance lesson learned throughout the course to practice Inquirer.
 
-Instruction
+## Instruction
 
 The user must create a basic repository including gitignore "node_modules" and license "MIT" from Github. The application requires Inquirer package, npm init, and package.json.
 
+## Github
 
-Hobbies
+https://github.com/LunaRossie/Readme-Generator--
 
-I like to explore places and try new cuisines. One of my favorite food is pasta.
 
-Language
+## Email address
+
+If you have any questions regarding this READme, please reach out via email "Terminalbash2022@outlook.com".
+
+
+## Video Link
+
+https://www.youtube.com/watch?v=0Qym1ID3vXg
+
+
+## Language
 
 I've learned JavaScript throughout the course, and I'm very excited to learn more.
 
-License
+## License
 
 I chose "MIT"
 
-Questions
+## Questions
 
 If you have any questions regarding this READme, please reach out via Github "https://github.com/LunaRossie/Readme-Generator--". https://www.youtube.com/watch?v=0Qym1ID3vXg
 
-Email address
 
-If you have any questions regarding this READme, please reach out via email "Terminalbash2022@outlook.com"
 
-Video Link
 
-https://www.youtube.com/watch?v=0Qym1ID3vXg
 
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,9 +1,56 @@
-function renderLicenseBadge(license){
-    const badges = {
-        ISC: '![License: MIT](https://github.com/LunaRossie/Readme-Generator--/issues)',
-    }
-    return badges [license]
-{
-function generateReadme(answers)
-    return `# ${answers.title}
+const generateMarkdown = Data => {
+    return `#{data.title}
+
+    ## Description 
+    ${data.description}
+
+    ## Table of Contents
+    * [Description] (#Description)
+    * [Instruction] (#Instruction)
+    * [Github] (#Github)
+    * [email address] (#email address)
+    * [Video Link] (#Video Link)
+    * [Language] (#Language)
+    * [License] (#License)
+    * [Questions] (#Questions)
+    
+    ## Description
+    ${data.install}
+
+    ## Instruction
+    ${data.install}
+
+    ## Github
+    ${data.install}
+
+    ## email address
+    ${data.install}
+
+    ## Video Link
+    ${data.install}
+
+    ## Language
+    ${data.install}
+
+    ## License
+    ${data.install}
+
+    ## Questions
+    If you have any questions regarding this READme, please reach out directly at ${data.email} or via Github "https://github.com/LunaRossie/Readme-Generator--".
+`;
 }
+
+module.exports = generateMarkdown;
+
+
+
+
+// function renderLicenseBadge(license){
+//     const badges = {
+//         ISC: '![License: MIT](https://github.com/LunaRossie/Readme-Generator--/issues)',
+//     }
+//     return badges [license]
+// {
+// function generateReadme(answers)
+//     return `# ${answers.title}
+// }
